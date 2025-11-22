371cefc761faef9c3c1fea882e90b681f328c95e
Ujwal Thakare <152703428+theujwalthakare@users.noreply.github.com>
2025-11-22 10:27:19 +0000

    updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,25 @@
-// Mobile nav toggle
+// Mobile nav toggl// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCgqUJvKCwJxUbZbbatC7rrkvId4mVj8Pk",
+    authDomain: "portfolio-hub-e2bff.firebaseapp.com",
+      projectId: "portfolio-hub-e2bff",
+        storageBucket: "portfolio-hub-e2bff.firebasestorage.app",
+          messagingSenderId: "27782433690",
+            appId: "1:27782433690:web:1215a44213312040c6c823",
+              measurementId: "G-Y8RJ0DJK4Z"
+              };
+
+              // Initialize Firebase
+              const app = initializeApp(firebaseConfig);
+              const analytics = getAnalytics(app);
+
 const navToggle = document.getElementById("navToggle");
 const navLinks = document.getElementById("navLinks");
 
